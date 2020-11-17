6f51923e5aa37dc8c3e1ad9cfa4f3ba6c121d488
cep104 <candicelizabethpeters@gmail.com>
2020-11-17 11:56:36 -0600

    commit tree 4a929241cbec8c071c96a0a1fb6b36399b4aaa58

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+// -----REMOVE MAIN ------------
+main.remove()
+// document.getElementById("main").remove()
+let newHeader = document.createElement("h1") 
+// create the h1
+newHeader.id = "victory"
+// assign id to h1
+document.body.appendChild(newHeader)
+// put h1 in the body of document
+newHeader.innerHTML = "Candice is the champion"
+
+
+
+
